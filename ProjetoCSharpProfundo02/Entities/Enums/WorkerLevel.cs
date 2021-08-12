630b4cd14d2fd2fbdceaf521b0331a3c18f8ac89
Ronaldo Silva <rona.jr96@gmail.com>
2021-08-12 09:15:20 -0300

    First exercise of the project 12082021

---
+++ ProjetoCSharpProfundo02/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoCSharpProfundo02.Entities.Enums
+{
+    public enum WorkerLevel : int
+    {
+        Junior,
+        MidLevel,
+        Senior
+    }
+}
